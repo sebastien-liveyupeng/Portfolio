15cf25c396cd602a51661d4883c35d4a02680424
sebastien-liveyupeng <sebastien.live-yu-peng@laplateforme.io>
2025-03-05 19:19:27 +0100

    ameliorations des bouttons

--- menu.js
+++ menu.js
@@ -9,3 +9,16 @@ prev.addEventListener('click' , function(){
     let items = document.querySelectorAll('.item')
     document.querySelector('.slide').prepend(items[items.length - 1])
 })
+document.addEventListener("DOMContentLoaded", function () {
+    const items = document.querySelectorAll(".item");
+
+    const showItems = () => {
+        items.forEach((item, index) => {
+            setTimeout(() => {
+                item.classList.add("show");
+            }, index * 150); // Décalage pour un effet progressif
+        });
+    };
+
+    showItems();
+});
